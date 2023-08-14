b8074e148921b06187eee051dbea2aa5c599c836
Nakamura <anderson.nakamura@atlanticsolutions.com.br>
2023-08-14 09:55:09 -0300

    structure sigeprev

---
+++ src/pages/P001-Home/index.tsx
@@ -0,0 +1,10 @@
+import Button from "../../components/shared/S001-Button";
+import "./styles.css";
+export default function Home() {
+  return (
+    <div className="P001-home">
+      <Button variant="secondary">base</Button>
+      <Button variant="primary">base1</Button>
+    </div>
+  );
+}
